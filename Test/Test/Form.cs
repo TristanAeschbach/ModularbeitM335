49f8c201334060fde1029e0401abbc40a1f6ff40
Tris10 <tristan.aeschbach@gmail.com>
2022-06-17 09:08:18 +0200

    Projektdateien hinzufügen.

---
+++ Test/Test/Form.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ModularbeitM335
+{
+    public partial class Form : ContentPage
+    {
+
+        public Form()
+        {
+            Entry name1 = new Entry { Placeholder = "Name" };
+            Entry producer1 = new Entry { Placeholder = "Producer" };
+            DatePicker datePicker = new DatePicker { Format = "dd/MM/yyyy" };
+            Content = new StackLayout
+            {
+                Children = {
+                    new StackLayout{
+                        Padding= new Thickness(30,10,30,0),
+                        BackgroundColor = Color.FromHex("#303030"),
+                        Children = {
+                            new Label{Text="Enter the Games Name", FontSize=16},
+                            name1,
+
+                            new Label{Text="Enter the Games Producer", FontSize=16},
+                            producer1,
+
+                            new Label{Text="Enter the Games Release Date", FontSize=16},
+                            datePicker,
+
+                            new Button {
+                                Text = "Submit",
+                                Command = new Command(() =>
+                                {
+                                    var name = name1.Text;
+                                    var producer = producer1.Text;
+                                    var date = datePicker.Date;
+
+                                    newEntry(name, producer, date);
+                                })
+                            }
+
+
+                        }
+
+                    }
+                }
+            };
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+
+        public void newEntry(String name, String producer, DateTime date)
+        {
+
+        }
+    }
+}
---
+++ Test/Test/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ModularbeitM335
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new StackLayout{
+                        Padding= new Thickness(30,10,30,0),
+                        BackgroundColor = Color.FromHex("#303030"),
+                        Children = {
+                            new Button {
+                                Text = "New Entry",
+                                Command = new Command(() =>
+                                {
+                                    
+                                })
+                            }
+                        }
+
+                    }
+                }
+            };
+        }
+    }
+}
